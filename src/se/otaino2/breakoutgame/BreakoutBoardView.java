a8d99e5f9704e0363f46cddbcf17befddd6b7310
Tomas Nilsson <tnilsson.x@gmail.com>
2013-05-01 11:17:08 +0200

    More infrastructure in place.
    Now possible to pause activity without crash. Also fixed nasty
    Android 4+ bug when surfaceDestroyed was invoked.

--- src/se/otaino2/breakoutgame/BreakoutBoardView.java
+++ src/se/otaino2/breakoutgame/BreakoutBoardView.java
@@ -17,6 +17,7 @@ import android.view.SurfaceView;
 
 public class BreakoutBoardView extends SurfaceView implements SurfaceHolder.Callback {
 
+    private static final String TAG = "BreakoutBoardView";
     /** The thread that actually draws the animation */
     private BreakoutBoardThread thread;
 
@@ -26,26 +27,28 @@ public class BreakoutBoardView extends SurfaceView implements SurfaceHolder.Call
         SurfaceHolder holder = getHolder();
         holder.addCallback(this);
 
-        thread = new BreakoutBoardThread(holder, context);
-
         // SurfaceView must have focus to get touch events
         setFocusable(true);
-    }
-    
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        thread.setSurfaceSize(width, height);
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-        // Start thread
+        Log.d(TAG, "Surface created, starting new thread...");
+        thread = new BreakoutBoardThread(holder, getContext());
         thread.setRunning(true);
         thread.start();
     }
 
     @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        Log.d(TAG, "Surface changed, resetting game...");
+        thread.setSurfaceSize(width, height);
+        thread.reset();
+    }
+
+    @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
+        Log.d(TAG, "Surface destroyed, trying to shut down thread...");
         boolean retry = true;
         thread.setRunning(false);
         while (retry) {
@@ -56,14 +59,21 @@ public class BreakoutBoardView extends SurfaceView implements SurfaceHolder.Call
                 // Do nothing
             }
         }
+        Log.d(TAG, "Thread's dead, baby. Thread's dead.");
     }
-    
-    public Thread getThread() {
+
+    @Override
+    public void onWindowFocusChanged(boolean hasWindowFocus) {
+        if (!hasWindowFocus)
+            thread.reset();
+    }
+
+    public BreakoutBoardThread getThread() {
         return thread;
     }
 
     class BreakoutBoardThread extends Thread {
-        
+
         private static final String TAG = "BreakoutBoardThread";
         private static final double NBR_OF_BLOCKS = 6;
         private SurfaceHolder surfaceHolder;
@@ -74,6 +84,7 @@ public class BreakoutBoardView extends SurfaceView implements SurfaceHolder.Call
         private int canvasHeight;
         private Background background;
         private List<Entity> gameEntities;
+        private long lastTime;
 
         public BreakoutBoardThread(SurfaceHolder surfaceHolder, Context context) {
             this.surfaceHolder = surfaceHolder;
@@ -83,7 +94,11 @@ public class BreakoutBoardView extends SurfaceView implements SurfaceHolder.Call
         public void setRunning(boolean running) {
             this.running = running;
         }
-        
+
+        public void reset() {
+            createEntities(canvasWidth, canvasHeight);
+        }
+
         @Override
         public void run() {
             while (running) {
@@ -92,7 +107,13 @@ public class BreakoutBoardView extends SurfaceView implements SurfaceHolder.Call
                     c = surfaceHolder.lockCanvas(null);
                     synchronized (surfaceHolder) {
                         updatePhysics();
-                        doDraw(c);
+                        // NOTE: In newer versions of Android (4+), it seems SurfaceHolder.lockCanvas() may return null whenever
+                        // SurfaceHolder.Callback.surfaceDestroyed() has been invoked. In earlier versions, a canvas was always
+                        // returned until SurfaceHolder.Callback.surfaceDestroyed() was FINISHED. See bug report:
+                        // https://code.google.com/p/android/issues/detail?id=38658
+                        if (c != null) {
+                            doDraw(c);
+                        }
                     }
                 } finally {
                     // do this in a finally so that if an exception is thrown
@@ -104,47 +125,56 @@ public class BreakoutBoardView extends SurfaceView implements SurfaceHolder.Call
                 }
             }
         }
-        
+
         public void setSurfaceSize(int width, int height) {
             synchronized (surfaceHolder) {
                 canvasWidth = width;
                 canvasHeight = height;
                 createEntities(width, height);
+                lastTime = System.currentTimeMillis() + 100;
             }
         }
 
         private void createEntities(int width, int height) {
             background = new Background(width, height);
             gameEntities = new ArrayList<Entity>();
-            double blockWidth = width / ( 1.1 * NBR_OF_BLOCKS + 0.1);
+            double blockWidth = width / (1.1 * NBR_OF_BLOCKS + 0.1);
             double blockHeight = 0.1 * blockWidth;
             for (int i = 0; i < NBR_OF_BLOCKS; i++) {
                 double blockX = i * (blockWidth + blockHeight) + blockHeight;
                 double blockY = 2 * blockHeight;
-                Block b = new Block((int)blockX, (int)blockY, (int)blockWidth, (int)blockHeight);
+                Block b = new Block((int) blockX, (int) blockY, (int) blockWidth, (int) blockHeight);
                 gameEntities.add(b);
             }
-            
-            double dotSide = blockHeight/2;
-            double dotX = width/2;
-            double dotY = height/2;
-            Dot d = new Dot((int)dotX, (int)dotY, (int)dotSide, (int)dotSide);
+
+            double dotSide = blockHeight / 2;
+            double dotX = width / 2;
+            double dotY = height / 2;
+            Dot d = new Dot((int) dotX, (int) dotY, (int) dotSide, (int) dotSide);
             gameEntities.add(d);
-            
+
             double paddleWidth = blockWidth * 2;
             double paddleHeight = blockHeight * 2;
             double paddleX = (width - paddleWidth) / 2;
             double paddleY = height - paddleHeight;
-            Paddle p = new Paddle((int)paddleX, (int)paddleY, (int)paddleWidth, (int)paddleHeight);
+            Paddle p = new Paddle((int) paddleX, (int) paddleY, (int) paddleWidth, (int) paddleHeight);
             gameEntities.add(p);
         }
 
-        // Update game entities for next iteration 
+        // Update game entities for next iteration
         private void updatePhysics() {
-            
+            long now = System.currentTimeMillis();
+
+            // Make sure we don't update physics unnecessary often
+            if (lastTime > now)
+                return;
+
+            double elapsed = (now - lastTime) / 1000.0;
+
+            lastTime = now;
         }
 
-        // Draws game entities on canvas. Must be run in 
+        // Draws game entities on canvas. Must be run in
         private void doDraw(Canvas c) {
             renderBackground(c);
             renderEntities(c);
@@ -154,7 +184,7 @@ public class BreakoutBoardView extends SurfaceView implements SurfaceHolder.Call
         private void renderBackground(Canvas c) {
             c.drawRect(background.getRect(), background.getPaint());
         }
-        
+
         private void renderEntities(Canvas c) {
             for (Entity e : gameEntities) {
                 c.drawRect(e.getRect(), e.getPaint());
--- src/se/otaino2/breakoutgame/MainActivity.java
+++ src/se/otaino2/breakoutgame/MainActivity.java
@@ -2,26 +2,36 @@ package se.otaino2.breakoutgame;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-
+    
+    private static final String TAG = "MainActivity";
     private BreakoutBoardView board;
-    private Thread thread;
 
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         board = (BreakoutBoardView) findViewById(R.id.gameboard);
-        thread = board.getThread();
     }
 	
 	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
     
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	    case R.id.action_reset:
+	        Log.d(TAG, "Resetting game");
+	        board.getThread().reset();
+	        break;
+	    }
+	    return false;
+	}
 }
